0a8a03526aec3dd39486b40cefc7ed33a00f50c9
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-04-13 17:09:42 +0200

    chore: debug fly deploy

--- src/server/pusher.ts
+++ src/server/pusher.ts
@@ -1,6 +1,9 @@
 import Pusher from "pusher";
 import { channelForUser } from "~/shared/Pusher";
 
+console.log("-------------------------");
+console.log(process.env);
+console.log("-------------------------");
 export const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID!,
   key: process.env.VITE_PUSHER_APP_KEY!,
