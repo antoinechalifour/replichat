8bfb416046923a26b846642773c207a6948b1c37
Antoine Chalifour <antoine.chalifour@gmail.com>
2025-04-13 17:05:28 +0200

    chore: fly deploy

--- src/server/pusher.ts
+++ src/server/pusher.ts
@@ -3,7 +3,7 @@ import { channelForUser } from "~/shared/Pusher";
 
 export const pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID!,
-  key: import.meta.env.VITE_PUSHER_APP_KEY,
+  key: process.env.VITE_PUSHER_APP_KEY!,
   secret: process.env.PUSHER_APP_SECRET!,
   cluster: process.env.PUSHER_APP_CLUSTER!,
   useTLS: true,
